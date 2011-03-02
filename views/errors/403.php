c07d4b53a267b19513e5c41db357de7fbcd9c941
Andrew Hart <afh@andrewfhart.com>
2011-03-02 02:20:08 +0000

    add a not-authorized page explaining why things are not visible and who to contact

---
+++ views/errors/403.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<center>
+<h3><strong>Not Authorized</strong></h3>
+</center>
+<div class="box">
+<p><strong>Please note:</strong> Some information on this website is restricted to users with the appropriate access permissions. The resource you attempted
+to access pertains to biomarker data that is currently being annotated or is under review. </p>
+</div>
+<center>
+<p>You may be able to view more by <a href="<?php echo SITE_ROOT?>/profile/login">logging in with your EDRN username and password</a>.</p>
+
+<span class="quiet">
+Please contact Heather Kincaid at the EDRN Informatics Center (edrn-ic@jpl.nasa.gov) with questions or for assistance.
+</span>
+</center>
